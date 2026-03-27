9cb5f2086de8a9f85e0430a37a30ea74513fa36e
Tanmay <tnmypthk@gmail.com>
2026-03-27 21:41:09 +0000

    daily weather model created

--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -7,20 +7,9 @@ DATE(TO_TIMESTAMP(STARTED_AT)) as DATE_STARTED_AT,
 HOUR(TO_TIMESTAMP(STARTED_AT)) as HOUR_STARTED_AT,
 case 
 
-when dayname(TO_TIMESTAMP(STARTED_AT)) in ('Sat', 'Sun')
-then 'Weekend'
-else 'Business Day'
-end as DAY_TYPE,
+{{day_type('STARTED_AT')}} as DAY_TYPE,
 
-case
- when MONTH(TO_TIMESTAMP(STARTED_AT)) in (12,1,2)
- then 'WINTER'
- when MONTH(TO_TIMESTAMP(STARTED_AT)) in (3,4,5)
- then 'SPRING'
- when MONTH(TO_TIMESTAMP(STARTED_AT)) in (6,7,8)
- then 'SUMMER'
- else 'AUTUMN'
- end as STATION_OF_YEAR
+{{get_season('STARTED_AT')}} as STATION_OF_YEAR
 
 
 from {{ source('demo', 'bike') }}
